9cc32d2c4a40c69d87c22d7c768f8d65442057c9
Danilo Chang <ray2501@gmail.com>
2021-07-25 11:57:13 +0800

    Update build.tcl

--- build.tcl
+++ build.tcl
@@ -1,14 +1,14 @@
 #!/usr/bin/tclsh
 
 set arch "noarch"
-set base "tcl-gremlin-0.1"
+set base "tcl-gremlin-0.2"
 
 set var2 [list git clone https://github.com/ray2501/tcl-gremlin.git $base]
 exec >@stdout 2>@stderr {*}$var2
 
 cd $base
 
-set var2 [list git checkout fe49e2ca5e506eacc5e4c8303727c3df045c0140]
+set var2 [list git checkout e3257f5942a4b8dab1bc509cfda34a1e1b0fe9b4]
 exec >@stdout 2>@stderr {*}$var2
 
 set var2 [list git reset --hard]
